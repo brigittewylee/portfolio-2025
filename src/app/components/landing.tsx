c6817649f7811cbc19b703ff681e8c54b957998c
brigittewylee <brigitte.leewy@gmail.com>
2025-05-11 00:57:39 -0400

    change landing format

--- src/app/components/landing.tsx
+++ src/app/components/landing.tsx
@@ -2,10 +2,10 @@ import styles from './styles.module.css';
 export default function Landing() {
 	return (
 	  <div className={`${styles.hoverBlurEffect} flex flex-col gap-0 justify-right`}>
-		<p className="w-full text-[170px]/30 font-[family-name:var(--arimo)] font-bold text-right m-0 pr-4">
-		  BRIGITTE LEE
+		<p className="w-full text-[170px]/40 font-[family-name:var(--arimo)] font-bold text-right m-0 pr-4">
+		  Brigitte Lee
 		</p>
-		<p className="text-[45px] font-[family-name:var(--arimo)] font-normal tracking-wider text-right mr-73">
+		<p className="text-[44px] font-[family-name:var(--arimo)] font-normal tracking-wider text-right mr-18">
 		  digital portfolio&apos;25  —interactive designer
 		</p>
 	  </div>
