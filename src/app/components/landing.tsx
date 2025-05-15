ad6faed4287edfe25465d00530b5c2e60e501754
brigittewylee <brigitte.leewy@gmail.com>
2025-05-15 02:49:37 -0400

    changed sub description

--- src/app/components/landing.tsx
+++ src/app/components/landing.tsx
@@ -1,11 +1,11 @@
 export default function Landing() {
 	return (
 	  <div className="flex flex-col gap-0 justify-right drop-shadow-[0px_0px_0.9px_rgba(0,0,0,1)] blur-section">
-		<p className="w-full text-[170px]/45 font-[family-name:var(--arimo)] font-bold text-right m-0 pr-4 fade-up">
+		<p className="w-full text-[170px]/45 font-[family-name:var(--arimo)] font-bold text-right m-0 pr-[2rem] fade-up">
 		  Brigitte Lee 
 		</p>
-		<p className="text-[44px] font-[family-name:var(--arimo)] font-normal tracking-wider text-right mr-19.5 animate-text fade-up">
-		  digital portfolio&apos;25  —designer//developer
+		<p className="text-[44px] font-[family-name:var(--arimo)] font-normal tracking-wider text-right mr-[9.5rem] animate-text fade-up">
+		  portfolio[2025]  —designer&developer
 		</p>
 	  </div>
 	);
