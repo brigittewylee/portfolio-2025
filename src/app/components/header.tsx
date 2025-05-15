a6f35dcc1f8f1e92a271e71df2f732e22889ef8d
brigittewylee <brigitte.leewy@gmail.com>
2025-05-15 02:50:01 -0400

    increase header blur

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -8,11 +8,11 @@ type HeaderProps = {
 export default function Header({headerRef}: HeaderProps) {
   return (
     <div ref={headerRef}
-        className="fixed top-0 right-0 left-0 w-full h-23 z-50 backdrop-blur-md bg-white/100 [mask-image:linear-gradient(to_bottom,black,transparent)] [mask-size:100%_100%]">
+        className="fixed top-0 right-0 left-0 w-full h-23 z-50 backdrop-blur-3xl bg-custom-white/100 [mask-image:linear-gradient(to_bottom,black,transparent)] [mask-size:100%_100%]">
 		<section className="flex text-m font-[family-name:var(--font-arimo)] font-bold drop-shadow-[0px_0px_0.9px_rgba(0,0,0,1)]">
 			<div className="w-1/2 flex p-4 text-left gap-1 fade-up">
 				<p>brigittelee &apos;25</p>
-          	</div>
+      </div>
 			<div className="w-1/2 flex p-4 mr-4 gap-30 z-[999] justify-end">
 				<div className="flex gap-0 fade-up">
 					<a href="#work"
