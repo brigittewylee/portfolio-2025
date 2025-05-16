2a99a1a572f6fc6815379dda5b3a77705b69dc7a
brigittewylee <brigitte.leewy@gmail.com>
2025-05-16 01:14:21 -0400

    header transition to black

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -7,15 +7,15 @@ type HeaderProps = {
 };
 
 export default function Header({headerRef, variant="default"}: HeaderProps) {
+  const underlineColor = variant === "alt" ? "after:bg-white" : "after:bg-black";
+  const textColor = variant === "alt" ? "text-custom-white" : "text-black";
+  const bgColor = variant === "alt" ? "bg-black" : "bg-custom-white";
+
   return (
     <div ref={headerRef}
-        className={`fixed top-0 w-full z-50 transition-all duration-500 backdrop-blur-3xl
-          [mask-image:linear-gradient(to_bottom,black_35%,transparent)] [mask-size:100%_100%]
-          ${
-            variant === "alt"
-              ? "bg-black/40 text-white"
-              : "bg-custom-white/90 text-black"
-          }`}>
+        className={`fixed top-0 w-full h-23 z-50 transition-all duration-500 backdrop-blur-3xl
+        [mask-image:linear-gradient(to_bottom,rgba(0,0,0,0.8)_35%,transparent)] [mask-size:100%_100%]
+        ${bgColor} ${textColor}`}>
 		<section className="flex text-m font-[family-name:var(--font-arimo)] font-bold drop-shadow-[0px_0px_0.9px_rgba(0,0,0,1)]">
 			<div className="w-1/2 flex p-4 text-left gap-1 fade-up">
 				<p>brigittelee &apos;25</p>
@@ -23,17 +23,17 @@ export default function Header({headerRef, variant="default"}: HeaderProps) {
 			<div className="w-1/2 flex p-4 mr-4 gap-30 z-[999] justify-end">
 				<div className="flex gap-0 fade-up">
 					<a href="#work"
-            className="relative inline-block cursor-pointer after:absolute after:left-0 after:top-[95%] after:w-0 after:h-[2px] after:bg-black after:transition-all after:duration-300 hover:after:w-full hover:after:drop-shadow-[0px_0px_0.9px_rgba(0,0,0,1)]">
+            className={`relative inline-block after:absolute after:left-0 after:top-[95%] after:w-0 after:h-[2px] ${underlineColor} after:transition-all after:duration-300 hover:after:w-full`}>
             work
           </a>
           <p>,</p>
 					<a href="#site-design"
-            className="relative inline-block after:absolute after:left-0 after:top-[95%] after:w-0 after:h-[2px] after:bg-black after:transition-all after:duration-300 hover:after:w-full">
+            className={`relative inline-block after:absolute after:left-0 after:top-[95%] after:w-0 after:h-[2px] ${underlineColor} after:transition-all after:duration-300 hover:after:w-full`}>
               site-design
           </a>
           <p>,</p>
 					<a href="#contact"
-            className="relative inline-block after:absolute after:left-0 after:top-[95%] after:h-[2px] after:w-0 after:bg-black after:transition-all after:duration-300 hover:after:w-full">
+            className={`relative inline-block after:absolute after:left-0 after:top-[95%] after:w-0 after:h-[2px] ${underlineColor} after:transition-all after:duration-300 hover:after:w-full`}>
             contact
           </a>
 				</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import gsap from "gsap";
 import { useEffect, useState, useRef} from "react";
 import Header from "./components/header";
 import Landing from "./components/landing";
@@ -9,20 +8,64 @@ import Design from "./components/design";
 import Contact from "./components/contact";
 import Cursor from "./components/cursor";
 import ScrollAnimations from "./scrollAnimations";
+import Lenis from '@studio-freight/lenis'
 
 export default function HomePage() {
   const headerRef = useRef<HTMLDivElement>(null); 
+  const designHeadRef = useRef<HTMLDivElement>(null);
   const contactRef = useRef<HTMLDivElement>(null);
+  const footerRef = useRef<HTMLDivElement>(null);
   const designRef = useRef<HTMLDivElement>(null);  
+  const [useAltHeader, setUseAltHeader] = useState(false); 
+
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      smooth: true,
+      lerp: 0.1,
+    })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
+
+  useEffect(() => { 
+    function onScroll() {
+      if (!designRef.current || !contactRef.current) return;
+
+      const designTop = designRef.current.getBoundingClientRect().top;
+      const designHeadHeight = designHeadRef.current.offsetHeight;
+      const contactTop = contactRef.current.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+      const adjustedDesignTop = designTop + designHeadHeight;
+
+      const inDesign = adjustedDesignTop < windowHeight * 0.5 && adjustedDesignTop > -designRef.current.offsetHeight;
+      const inContact = contactTop < windowHeight * 0.5 && contactTop > -contactRef.current.offsetHeight;
+
+
+      if (inDesign || inContact) {
+        setUseAltHeader(true);
+      } else {
+        setUseAltHeader(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
   return (
     <div className="scroll-smooth">
-      <Cursor headerRef={headerRef} contactRef={contactRef}/>
+      <Cursor headerRef={headerRef} variant={useAltHeader ? "alt" : "default"} contactRef={contactRef} footerRef={footerRef}/>
       <ScrollAnimations />
 
       <div>
         <header className="w-full">
-            <Header headerRef={headerRef}/>
+            <Header headerRef={headerRef} variant={useAltHeader ? "alt" : "default"}/>
         </header>
       </div>
 
@@ -39,12 +82,12 @@ export default function HomePage() {
       </section>
 
       <div className="bg-black">
-        <section ref={designRef} id="site-design" className="box pb-20">
-            <Design />
+        <section id="site-design" className="box pb-20">
+            <Design designRef={designRef} designHeadRef={designHeadRef}/>
         </section>
 
         <section id="contact" className="box mt-0">
-          <Contact contactRef={contactRef}/>
+          <Contact contactRef={contactRef} footerRef={footerRef}/>
         </section>
       </div>
     </div>
