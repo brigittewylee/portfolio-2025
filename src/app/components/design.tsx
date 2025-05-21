49b369e6a8cad94acfbafc3d1c531a63457d1c29
brigittewylee <brigitte.leewy@gmail.com>
2025-05-21 00:01:17 -0400

    better photos

--- src/app/components/design.tsx
+++ src/app/components/design.tsx
@@ -18,26 +18,26 @@ export default function Design({ designRef, designHeadRef }: DesignProp) {
       <div ref={designRef} 
             className="bg-black inset-0 z-10 h-auto w-full text-custom-white">
         <div className="flex flex-col">
-          <div className="bg-black pt-[8rem] pb-[3rem] font-[family-name:var(--font-arimo)] text-3xl not-italic drop-shadow-[0px_0px_0.5px_rgba(0,0,0,1)]">
+          <div className="bg-black pt-[8rem] pb-[3rem] font-[family-name:var(--font-arimo)] text-3xl drop-shadow-[0px_0px_0.5px_rgba(0,0,0,1)]">
             <div className="fade-up flex">
               <div className="flex-1 pl-20">
                 <div className="text-7xl">FONTS</div>
                 <p className="pl-10">
-                  <span className="font-normal">ARIMO</span> - ARIMO - <span className="italic">ARIMO</span>
+                  ARIMO - <span className="italic">ARIMO</span> - <span className="font-bold">ARIMO</span>
                 </p>
 
                 <p className="pl-10 font-[family-name:var(--font-bodoni-moda)]">
-                  BODONI MODA - <span className="italic">BODONI MODA</span>
+                  BODONI MODA - <span className="italic font-bold">BODONI MODA</span>
                 </p>
 
                 <div className="text-7xl pt-5">PALETTE</div>
                 <div className="pl-10 flex">
                   <div className="relative box-content bg-custom-white bg-ececec border border-outline-white h-[215px] w-[190px] mr-2">
-                    <p className="absolute text-[20px] text-black bottom-2 right-4">#ECECEC</p>
+                    <p className="absolute text-[20px] text-black font-bold bottom-2 right-4">#ECECEC</p>
                   </div>
 
                   <div className="relative box-content border border-outline-black h-[215px] w-[190px]">
-                    <p className="absolute text-[20px] text-custom-white bottom-2 right-4">#000000</p>
+                    <p className="absolute text-[20px] font-bold bottom-2 right-4">#000000</p>
                   </div>                  
                 </div>
               </div>
