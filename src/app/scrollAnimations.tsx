b158e030e7431d39fc3a7a549f96830bd49ff4e6
brigittewylee <brigitte.leewy@gmail.com>
2025-05-20 23:53:45 -0400

    fix formatting

--- src/app/scrollAnimations.tsx
+++ src/app/scrollAnimations.tsx
@@ -15,39 +15,39 @@ export default function ScrollAnimations() {
       gsap.from(split.chars, {
         scrollTrigger: {
           trigger: el,
-          start: "top 80%",
+          start:"top 80%",
         },
         opacity: 0,
         y: 10,
         stagger: 0.05,
         ease: "power3.out",
-        duration: 0.2,
+        duration: 0.2
       });
     });
 
     const fadeUps = document.querySelectorAll(".fade-up");
     fadeUps.forEach((el, index) => {
-        gsap.fromTo(
-          el,
-          {
-            opacity: 0,
-            y: 50,
+      gsap.fromTo(
+        el,
+        {
+          opacity:0,
+          y:50,
+        },
+        {
+          scrollTrigger: {
+            trigger: el,
+            start: "top bottom",
+            toggleActions: "play none none none",
+            once: true,
           },
-          {
-            scrollTrigger: {
-              trigger: el,
-              start: "top bottom",
-              toggleActions: "play none none none",
-              once: true,
-            },
-            opacity: 1,
-            y: 0,
-            duration: 1,
-            ease: "power3.out",
-            delay: index * 0.05,
-          }
-        );
-      });
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          ease: "power3.out",
+          delay: index * 0.05,
+
+        });
+    });
 
     const marquees_left = document.querySelectorAll(".marquee-left");
     marquees_left.forEach((el) => {
@@ -76,7 +76,7 @@ export default function ScrollAnimations() {
         },
       });
     });
-    
+
     return () => ScrollTrigger.getAll().forEach((t) => t.kill());
   }, []);
 
